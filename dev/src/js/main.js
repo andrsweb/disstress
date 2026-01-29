e83cb1fcd47b5b9578f92d5e25f99c76d95ac8d8
andrsweb <d3vv.proo@gmail.com>
2026-01-29 20:51:26 +0700

    default project upd

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -1,7 +1,6 @@
 import './common/common';
 import './common/tabs';
 // Components && Sections js
-import '../html/components/common/header/header-menu';
 import './contact-panel.js';
 import '../html/sections/index/map/map';
 import './swiper.js';
