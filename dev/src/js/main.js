bc48a213726a07b24f08fd32a3dab7e2a037d37c
andrsweb <d3vv.proo@gmail.com>
2026-01-30 14:52:48 +0700

    create unit page

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -2,7 +2,7 @@ import './common/common';
 import './common/tabs';
 // Components && Sections js
 import './contact-panel';
-import '../html/sections/index/map/map';
+import '../html/sections/unit/map/map';
 import './swiper';
 import './dropdowns';
 import './filters';
