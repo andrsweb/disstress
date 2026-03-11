7a8803d6f2f14ef6e904d1413b5c8193856b71f8
ns <NSukonny@gmail.com>
2026-03-11 21:26:47 +0300

    breadcrumbs and single property page

--- dev/src/js/filters.js
+++ dev/src/js/filters.js
@@ -1,5 +1,3 @@
-import {loadSearchData} from './common/common.js'
-
 document.addEventListener('DOMContentLoaded', () => {
     'use strict'
     void initSearchResultsFilters()
@@ -251,6 +249,21 @@ const updatePropertiesList = () => {
         formData.append('baths', bathsValueInput.value);
     }
 
+    let currentUrl = new URL(window.location.href);
+    
+    formData.forEach((value, key) => {
+        if ('action' === key || '_ajax_nonce' === key) return;
+
+        if ('all' === value || 'undefined' === value || 0 === value.length) {
+            currentUrl.searchParams.delete(key);
+            return;
+        }
+
+        currentUrl.searchParams.set(key, value);
+    });
+
+    formData.append('current_href', currentUrl.href);
+
     fetch(ajax_object.ajax_url, {
         method: 'POST',
         body: formData,
@@ -264,6 +277,12 @@ const updatePropertiesList = () => {
                 resultsBlock.innerHTML = response.data.properties;
                 h2Block.innerHTML = response.data.properties_found;
             }
+            console.log(currentUrl.href);
+            history.pushState(
+                currentUrl.hash,
+                '',
+                currentUrl.href
+            );
             setTimeout(() => {
                 resultsBlock.classList.remove('preloader');
                 document.dispatchEvent(new Event('ajaxComplete'));
